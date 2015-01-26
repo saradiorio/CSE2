9120d7a362344b587d75975d7aa5405a50c25a89
saradiorio <sgd217@lehigh.edu>
2015-01-26 05:16:35 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+////////////////////////////
+//Sara D'Iorio
+//hw01
+//Welcome Class Java Program
+//
+//  first compile program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[ ] args) {
+        
+// print the statement with box and arrows around
+    System.out.println("  -----------       ");
+    System.out.println("  | WELCOME |       ");
+    System.out.println("  -----------       ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-S--G--D--2--1--7->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("I am a classically trained singer. I spend most of my after clas time at choir rehearsals,");
+    System.out.println("which makes it hard to do any other extra-curricular activities.");
+    
+    //test/testtest
+    
+    }
+}
